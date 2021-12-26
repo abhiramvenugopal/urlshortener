be7c2bfc8248866ca44f9759e28b65caf028388c
abhiramvenugopal <abhiram.venugopal007@gmail.com>
2021-12-26 12:02:32 +0530

    implemented utility functions and completed the basic api

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ const express=require("express");
 const mongoose=require("mongoose");
 const dotenv = require('dotenv');
 const cors=require("cors")
+const urlAPI=require('./api/urlApi')
+const redirectAPI=require('./api/redirectApi')
+var bodyParser = require('body-parser')
 
 
 const app=express();
-app.use(cors())
 dotenv.config();
+
+mongoose.connect(process.env.DB)
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+app.use("/api/v1/url/",urlAPI);
+app.use("/url/",redirectAPI);
 
 
 const PORT=process.env.PORT || 8080
---
+++ utility/utility.js
@@ -0,0 +1,4 @@
+function createUrlId(){
+    return Date.now().toString(36) + Math.random().toString(36).substr(2);
+}
+module.exports = { createUrlId }
