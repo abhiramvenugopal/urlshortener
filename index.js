f50d762b040b55d367a24c870425a24fea34f134
abhiramvenugopal <abhiram.venugopal007@gmail.com>
2021-12-26 13:28:11 +0530

    added basic swagger setup

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const cors=require("cors")
 const urlAPI=require('./api/urlApi')
 const redirectAPI=require('./api/redirectApi')
 var bodyParser = require('body-parser')
+const swaggerJsDoc=require('swagger-jsdoc')
+const swaggerUI=require('swagger-ui-express')
 
 
 const app=express();
@@ -14,6 +16,27 @@ mongoose.connect(process.env.DB)
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
+
+
+const swaggerInfo={
+    swaggerDefinition: {
+        info: {
+          version: "1.0.0",
+          title: "URL Shortner API",
+          description: "API for converting long URLs with UTM tracking",
+          contact: {
+            name: "Abhiram Venugopal",
+            email: "abhiram.venugopal007@gmail.com"
+          },
+          servers: ["https://urlshortenerapp77.herokuapp.com/"]
+        }
+      },
+      apis: ["./api/*.js"]
+}
+
+const swaggerDocs=swaggerJsDoc(swaggerInfo)
+app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocs))
+
 
 //API for Creating short URL
 
